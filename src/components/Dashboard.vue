99d46c5d8c54b94bf5e3fd351ab3e01537fede4b
mal90 <SalimM@synergenhealth.com>
2017-10-27 00:54:25 -0500

    Add bootsrap-vue to the boiler plate

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="container topmargin" id="dashboard">
-        <div class="col-md-12">
-            <div class="panel panel-info">
-                <div class="panel-heading">Dashboard</div>
-                <div class="panel-body">
-                    <div class="col-md-4">
-                    </div>
-                    <div class="col-md-4">
-                        <div class="btn-group">
-                            <router-link type="button" class="btn btn-primary" v-bind:to="'/'">Home</router-link>
-                            <router-link type="button" class="btn btn-success" v-bind:to="'/products'">Products</router-link>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        <div class="text-center my-3">
+        <b-btn v-b-tooltip.hover title="Tooltip content">Hover Me</b-btn>
+</div>
+  </div>
 </template>
 
 <script>
